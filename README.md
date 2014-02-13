6d8b23c86a58e9f7d9e0e60610ca8a4c350283a3
monofone <s.rohweder@blage.net>
2014-02-14 00:17:27 +0100

    Changed to ansible installer for managing ansible roles via composer

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# A Multi-Framework [Composer](http://getcomposer.org) Library Installer
+# Ansible Installer
 
-[![Build Status](https://secure.travis-ci.org/composer/installers.png)](http://travis-ci.org/composer/installers)
+This is originally taken form the [composer/installers](https://github.com/composer/installers) and
+adapted to work as an [ansible](http://www.ansible.com/home) role installer.
+
+[![Build Status](https://secure.travis-ci.org/monofone/installers.png)](http://travis-ci.org/monofone/installers)
 
 This is for PHP package authors to require in their `composer.json`. It will
 install their package to the correct location based on the specified package
@@ -10,20 +13,6 @@ The goal of `installers` is to be a simple package type to install path map.
 Users can also customize the install path per package and package authors can
 modify the package name upon installing.
 
-`installers` isn't intended on replacing all custom installers. If your
-package requires special installation handling then by all means, create a
-custom installer to handle it.
-
-**Natively Supported Frameworks**:
-
-The following frameworks natively work with Composer and will be
-installed to the default `vendor` directory. `composer/installers`
-is not needed to install packages with these frameworks:
-
-* Aura
-* Symfony2
-* Yii
-* Yii2
 
 **Current Supported Package Types**:
 
@@ -33,71 +22,39 @@ is not needed to install packages with these frameworks:
 
 | Framework    | Types
 | ---------    | -----
-| AGL          | `agl-module`
-| AnnotateCms  | `annotatecms-module`<br>`annotatecms-component`<br>`annotatecms-service`
-| CakePHP 2+   | **`cakephp-plugin`**
-| CodeIgniter  | `codeigniter-library`<br>`codeigniter-third-party`<br>`codeigniter-module`
-| concrete5    | `concrete5-block`<br>`concrete5-package`<br>`concrete5-theme`
-| Craft        | `craft-plugin`
-| Croogo       | `croogo-plugin`<br>`croogo-theme`
-| Drupal       | <b>`drupal-module`<br>`drupal-theme`</b><br>`drupal-profile`<br>`drupal-drush`
-| Elgg         | `elgg-plugin`
-| FuelPHP v1.x | `fuel-module`<br>`fuel-package`<br/>`fuel-theme`
-| Hurad        | `hurad-plugin`<br>`hurad-theme`
-| Joomla       | `joomla-component`<br>`joomla-module`<br>`joomla-template`<br>`joomla-plugin`<br>`joomla-library`
-| Kohana       | **`kohana-module`**
-| Laravel      | `laravel-library`
-| Lithium      | **`lithium-library`<br>`lithium-source`**
-| Magento      | `magento-library`<br>`magento-skin`<br>`magento-theme`
-| Mako         | `mako-package`
-| MODX Evo     | `modxevo-snippet`<br>`modxevo-plugin`<br>`modxevo-module`<br>`modxevo-template`<br>`modxevo-lib`
-| MediaWiki    | `mediawiki-extension`
-| OXID         | `oxid-module`
-| MODULEWork   | `modulework-module`
-| Piwik        | `piwik-plugin`
-| phpBB        | `phpbb-extension`<br>`phpbb-style`<br>`phpbb-language`
-| PPI          | **`ppi-module`**
-| shopware     | `shopware-backend-plugin`<br/>`shopware-core-plugin`<br/>`shopware-frontend-plugin`<br/>`shopware-theme`
-| SilverStripe | `silverstripe-module`<br>`silverstripe-theme`
-| symfony1     | **`symfony1-plugin`**
-| TYPO3 Flow   | `typo3-flow-package`<br>`typo3-flow-framework`<br>`typo3-flow-plugin`<br>`typo3-flow-site`<br>`typo3-flow-boilerplate`<br>`typo3-flow-build`
-| TYPO3 CMS    | `typo3-cms-extension`
-| WordPress    | <b>`wordpress-plugin`<br>`wordpress-theme`</b><br>`wordpress-muplugin`
-| Zend         | `zend-library`<br>`zend-extra`<br>`zend-module`
-| Zikula       | `zikula-module`<br>`zikula-theme`
+| ansible      | `ansible-role`
+
 
 ## Example `composer.json` File
 
-This is an example for a CakePHP plugin. The only important parts to set in your
-composer.json file are `"type": "cakephp-plugin"` which describes what your
-package is and `"require": { "composer/installers": "~1.0" }` which tells composer
+This is an example for a ansible role. The only important parts to set in your
+composer.json file are `"type": "ansible-role"` which describes what your
+package is and `"require": { "ansible/installer": "~1.0" }` which tells composer
 to load the custom installers.
 
 ```json
 {
-    "name": "you/ftp",
-    "type": "cakephp-plugin",
+    "name": "you/mysql",
+    "type": "ansible-role",
     "require": {
-        "composer/installers": "~1.0"
+        "ansible/installer": "~1.0"
     }
 }
 ```
 
-This would install your package to the `Plugin/Ftp/` folder of a CakePHP app
-when a user runs `php composer.phar install`.
-
-So submit your packages to [packagist.org](http://packagist.org)!
+This would install your role to the `provisioning/roles/common/` folder when a
+user runs `php composer.phar install`.
 
 ## Custom Install Paths
 
-If you are consuming a package that uses the `composer/installers` you can
+If you are consuming a package that uses the `ansible/installer` you can
 override the install path with the following extra in your `composer.json`:
 
 ```json
 {
     "extra": {
         "installer-paths": {
-            "your/custom/path/{$name}/": ["shama/ftp", "vendor/package"]
+            "your/custom/path/{$name}/": ["ansible/web", "vendor/package"]
         }
     }
 }
@@ -109,7 +66,7 @@ A package type can have a custom installation path with a `type:` prefix.
 {
     "extra": {
         "installer-paths": {
-            "your/custom/path/{$name}/": ["type:wordpress-plugin"]
+            "your/custom/path/{$name}/": ["type:ansible-role"]
         }
     }
 }
@@ -123,49 +80,22 @@ variables to use in your paths are: `{$name}`, `{$vendor}`, `{$type}`.
 If you're a package author and need your package to be named differently when
 installed consider using the `installer-name` extra.
 
-For example you have a package named `shama/cakephp-ftp` with the type
-`cakephp-plugin`. Installing with `composer/installers` would install to the
-path `Plugin/CakephpFtp`. Due to the strict naming conventions, you as a
+For example you have a package named `you/mysql` with the type
+`ansible-role`. Installing with `ansible/installer` would install to the
+path `provisioning/roles/mysql`. Due to the strict naming conventions, you as a
 package author actually need the package to be named and installed to
-`Plugin/Ftp`. Using the following config within your **package** `composer.json`
+`provisioning/roles/ownmysql`. Using the following config within your **package** `composer.json`
 will allow this:
 
 ```json
 {
-    "name": "shama/cakephp-ftp",
-    "type": "cakephp-plugin",
+    "name": "you/mysql",
+    "type": "ansible-role",
     "extra": {
-        "installer-name": "Ftp"
+        "installer-name": "ownmysql"
     }
 }
 ```
 
 Please note the name entered into `installer-name` will be the final and will
 not be inflected.
-
-## Contribute!
-
-* [Fork and clone](https://help.github.com/articles/fork-a-repo).
-* Run the command `php composer.phar install --dev` to install the dev
-  dependencies. See [Composer](https://github.com/composer/composer#installation--usage).
-* Use the command `phpunit` to run the tests. See [PHPUnit](http://phpunit.de).
-* Create a branch, commit, push and send us a
-  [pull request](https://help.github.com/articles/using-pull-requests).
-
-To ensure a consistent code base, you should make sure the code follows the
-[Coding Standards](http://symfony.com/doc/2.0/contributing/code/standards.html)
-which we borrowed from Symfony.
-
-If you would like to help, please take a look at the list of
-[issues](https://github.com/composer/installers/issues).
-
-### Should we allow dynamic package types or paths? No.
-What are they? The ability for a package author to determine where a package
-will be installed either through setting the path directly in their
-`composer.json` or through a dynamic package type: `"type":
-"framework-install-here"`.
-
-It has been proposed many times. Even implemented once early on and then
-removed. `installers` won't do this because it would allow a single package
-author to wipe out entire folders without the user's consent. That user would
-then come here to yell at us.
--- tests/Composer/Installers/Test/InstallerTest.php
+++ tests/Composer/Installers/Test/InstallerTest.php
@@ -84,60 +84,7 @@ class InstallerTest extends TestCase
     public function dataForTestSupport()
     {
         return array(
-            array('agl-module', true),
-            array('annotatecms-module', true),
-            array('annotatecms-component', true),
-            array('annotatecms-service', true),
-            array('cakephp', false),
-            array('cakephp-', false),
-            array('cakephp-app', false),
-            array('cakephp-plugin', true),
-            array('codeigniter-app', false),
-            array('codeigniter-library', true),
-            array('codeigniter-third-party', true),
-            array('codeigniter-module', true),
-            array('concrete5-block', true),
-            array('concrete5-package', true),
-            array('concrete5-theme', true),
-            array('craft-plugin', true),
-            array('croogo-plugin', true),
-            array('croogo-theme', true),
-            array('drupal-module', true),
-            array('elgg-plugin', true),
-            array('fuel-module', true),
-            array('fuel-package', true),
-            array('hurad-plugin', true),
-            array('hurad-theme', true),
-            array('joomla-library', true),
-            array('kohana-module', true),
-            array('laravel-library', true),
-            array('lithium-library', true),
-            array('magento-library', true),
-            array('mako-package', true),
-            array('modxevo-snippet', true),
-            array('modxevo-plugin', true),
-            array('modxevo-module', true),
-            array('modxevo-template', true),
-            array('modxevo-lib', true),
-            array('mediawiki-extension', true),
-            array('modulework-module', true),
-            array('piwik-plugin', true),
-            array('phpbb-extension', true),
-            array('ppi-module', true),
-            array('shopware-backend-plugin', true),
-            array('shopware-core-plugin', true),
-            array('shopware-frontend-plugin', true),
-            array('shopware-theme', true),
-            array('silverstripe-module', true),
-            array('silverstripe-theme', true),
-            array('symfony1-plugin', true),
-            array('typo3-flow-plugin', true),
-            array('typo3-cms-extension', true),
-            array('wordpress-plugin', true),
-            array('wordpress-core', false),
-            array('zend-library', true),
-            array('zikula-module', true),
-            array('zikula-theme', true),
+            array('ansible-role', true),
         );
     }
 
@@ -162,68 +109,7 @@ class InstallerTest extends TestCase
     public function dataForTestInstallPath()
     {
         return array(
-            array('agl-module', 'More/MyTestPackage/', 'agl/my_test-package'),
-            array('annotatecms-module', 'addons/modules/my_module/', 'vysinsky/my_module'),
-            array('annotatecms-component', 'addons/components/my_component/', 'vysinsky/my_component'),
-            array('annotatecms-service', 'addons/services/my_service/', 'vysinsky/my_service'),
-            array('cakephp-plugin', 'Plugin/Ftp/', 'shama/ftp'),
-            array('codeigniter-library', 'application/libraries/my_package/', 'shama/my_package'),
-            array('codeigniter-module', 'application/modules/my_package/', 'shama/my_package'),
-            array('concrete5-block', 'blocks/concrete5_block/', 'remo/concrete5_block'),
-            array('concrete5-package', 'packages/concrete5_package/', 'remo/concrete5_package'),
-            array('concrete5-theme', 'themes/concrete5_theme/', 'remo/concrete5_theme'),
-            array('craft-plugin', 'craft/plugins/my_plugin/', 'mdcpepper/my_plugin'),
-            array('croogo-plugin', 'Plugin/Sitemaps/', 'fahad19/sitemaps'),
-            array('croogo-theme', 'View/Themed/Readable/', 'rchavik/readable'),
-            array('drupal-module', 'modules/my_module/', 'shama/my_module'),
-            array('drupal-theme', 'themes/my_module/', 'shama/my_module'),
-            array('drupal-profile', 'profiles/my_module/', 'shama/my_module'),
-            array('drupal-drush', 'drush/my_module/', 'shama/my_module'),
-            array('elgg-plugin', 'mod/sample_plugin/', 'test/sample_plugin'),
-            array('fuel-module', 'fuel/app/modules/module/', 'fuel/module'),
-            array('fuel-package', 'fuel/packages/orm/', 'fuel/orm'),
-            array('hurad-plugin', 'Plugin/Akismet/', 'atkrad/akismet'),
-            array('hurad-theme', 'View/Themed/Hurad2013/', 'atkrad/Hurad2013'),
-            array('joomla-plugin', 'plugins/my_plugin/', 'shama/my_plugin'),
-            array('kohana-module', 'modules/my_package/', 'shama/my_package'),
-            array('laravel-library', 'libraries/my_package/', 'shama/my_package'),
-            array('lithium-library', 'libraries/li3_test/', 'user/li3_test'),
-            array('magento-library', 'lib/foo/', 'test/foo'),
-            array('modxevo-snippet', 'assets/snippets/my_snippet/', 'shama/my_snippet'),
-            array('modxevo-plugin', 'assets/plugins/my_plugin/', 'shama/my_plugin'),
-            array('modxevo-module', 'assets/modules/my_module/', 'shama/my_module'),
-            array('modxevo-template', 'assets/templates/my_template/', 'shama/my_template'),
-            array('modxevo-lib', 'assets/lib/my_lib/', 'shama/my_lib'),
-            array('mako-package', 'app/packages/my_package/', 'shama/my_package'),
-            array('mediawiki-extension', 'extensions/APC/', 'author/APC' ),
-            array('mediawiki-extension', 'extensions/UploadWizard/', 'author/upload-wizard' ),
-            array('mediawiki-extension', 'extensions/SyntaxHighlight_GeSHi/', 'author/syntax-highlight_GeSHi' ),
-            array('modulework-module', 'modules/my_package/', 'shama/my_package'),
-            array('piwik-plugin', 'plugins/VisitSummary/', 'shama/visit-summary'),
-            array('phpbb-extension', 'ext/test/foo/', 'test/foo'),
-            array('phpbb-style', 'styles/foo/', 'test/foo'),
-            array('phpbb-language', 'language/foo/', 'test/foo'),
-            array('ppi-module', 'modules/foo/', 'test/foo'),
-            array('shopware-backend-plugin', 'engine/Shopware/Plugins/Local/Backend/ShamaMyBackendPlugin/', 'shama/my-backend-plugin'),
-            array('shopware-core-plugin', 'engine/Shopware/Plugins/Local/Core/ShamaMyCorePlugin/', 'shama/my-core-plugin'),
-            array('shopware-frontend-plugin', 'engine/Shopware/Plugins/Local/Frontend/ShamaMyFrontendPlugin/', 'shama/my-frontend-plugin'),
-            array('shopware-theme', 'templates/my_theme/', 'shama/my-theme'),
-            array('silverstripe-module', 'my_module/', 'shama/my_module'),
-            array('silverstripe-module', 'sapphire/', 'silverstripe/framework', '2.4.0'),
-            array('silverstripe-module', 'framework/', 'silverstripe/framework', '3.0.0'),
-            array('silverstripe-module', 'framework/', 'silverstripe/framework', '3.0.0-rc1'),
-            array('silverstripe-module', 'framework/', 'silverstripe/framework', 'my/branch'),
-            array('silverstripe-theme', 'themes/my_theme/', 'shama/my_theme'),
-            array('symfony1-plugin', 'plugins/sfShamaPlugin/', 'shama/sfShamaPlugin'),
-            array('symfony1-plugin', 'plugins/sfShamaPlugin/', 'shama/sf-shama-plugin'),
-            array('typo3-flow-package', 'Packages/Application/my_package/', 'shama/my_package'),
-            array('typo3-flow-build', 'Build/my_package/', 'shama/my_package'),
-            array('typo3-cms-extension', 'typo3conf/ext/my_extension/', 'shama/my_extension'),
-            array('wordpress-plugin', 'wp-content/plugins/my_plugin/', 'shama/my_plugin'),
-            array('wordpress-muplugin', 'wp-content/mu-plugins/my_plugin/', 'shama/my_plugin'),
-            array('zend-extra', 'extras/library/zend_test/', 'shama/zend_test'),
-            array('zikula-module', 'modules/my-test_module/', 'my/test_module'),
-            array('zikula-theme', 'themes/my-test_theme/', 'my/test_theme'),
+            array('ansible-role', 'provisioning/roles/common/', 'ansible/common'),
         );
     }
 
@@ -241,94 +127,6 @@ class InstallerTest extends TestCase
 
         $package->setType('cakephp-whoops');
         $result = $installer->getInstallPath($package);
-    }
-
-    /**
-     * testCustomInstallPath
-     */
-    public function testCustomInstallPath()
-    {
-        $installer = new Installer($this->io, $this->composer);
-        $package = new Package('shama/ftp', '1.0.0', '1.0.0');
-        $package->setType('cakephp-plugin');
-        $consumerPackage = new RootPackage('foo/bar', '1.0.0', '1.0.0');
-        $this->composer->setPackage($consumerPackage);
-        $consumerPackage->setExtra(array(
-            'installer-paths' => array(
-                'my/custom/path/{$name}/' => array(
-                    'shama/ftp',
-                    'foo/bar',
-                ),
-            ),
-        ));
-        $result = $installer->getInstallPath($package);
-        $this->assertEquals('my/custom/path/Ftp/', $result);
-    }
-
-    /**
-     * testCustomInstallerName
-     */
-    public function testCustomInstallerName() {
-        $installer = new Installer($this->io, $this->composer);
-        $package = new Package('shama/cakephp-ftp-plugin', '1.0.0', '1.0.0');
-        $package->setType('cakephp-plugin');
-        $package->setExtra(array(
-            'installer-name' => 'FTP',
-        ));
-        $result = $installer->getInstallPath($package);
-        $this->assertEquals('Plugin/FTP/', $result);
-    }
-
-    /**
-     * testCustomTypePath
-     */
-    public function testCustomTypePath() {
-        $installer = new Installer($this->io, $this->composer);
-        $package = new Package('slbmeh/my_plugin', '1.0.0', '1.0.0');
-        $package->setType('wordpress-plugin');
-        $consumerPackage = new RootPackage('foo/bar', '1.0.0', '1.0.0');
-        $this->composer->setPackage($consumerPackage);
-        $consumerPackage->setExtra(array(
-            'installer-paths' => array(
-                'my/custom/path/{$name}/' => array(
-                    'type:wordpress-plugin'
-                ),
-            ),
-        ));
-        $result = $installer->getInstallPath($package);
-        $this->assertEquals('my/custom/path/my_plugin/', $result);
-    }
-
-    /**
-     * testNoVendorName
-     */
-    public function testNoVendorName()
-    {
-        $installer = new Installer($this->io, $this->composer);
-        $package = new Package('sfPhpunitPlugin', '1.0.0', '1.0.0');
-
-        $package->setType('symfony1-plugin');
-        $result = $installer->getInstallPath($package);
-        $this->assertEquals('plugins/sfPhpunitPlugin/', $result);
-    }
-
-    /**
-     * testTypo3Inflection
-     */
-    public function testTypo3Inflection()
-    {
-        $installer = new Installer($this->io, $this->composer);
-        $package = new Package('typo3/fluid', '1.0.0', '1.0.0');
-
-        $package->setAutoload(array(
-            'psr-0' => array(
-                'TYPO3\\Fluid' => 'Classes',
-            ),
-        ));
-
-        $package->setType('typo3-flow-package');
-        $result = $installer->getInstallPath($package);
-        $this->assertEquals('Packages/Application/TYPO3.Fluid/', $result);
     }
 
     public function testUninstallAndDeletePackageFromLocalRepo()
